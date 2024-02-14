2811414b8632ec7c69e682a19895c588c94a9c94
BlackBuda26 <jsgutierrezmaya@gmail.com>
2024-02-14 10:44:01 -0500

    Conexión Base Datos PostgreSQL

--- pruebas.py
+++ pruebas.py
@@ -1,1 +1,12 @@
-print("Prueba Repositorio") 
+import psycopg2
+
+
+# Conexión a la base de datos PostgreSQL en ElephantSQL
+conn = psycopg2.connect(
+    dbname="tnoinmky",
+    user="tnoinmky",
+    password="O64c7I4rRU8JYbAjQzXkI66n9grGB_16",
+    host="jelani.db.elephantsql.com",
+    port="5432"  # Puerto predeterminado para PostgreSQL
+)
+
